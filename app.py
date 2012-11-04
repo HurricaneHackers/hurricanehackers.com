03d6db16cb13e99c47320b4ea8b6c7fd02dbb434
B1naryTh1ef <b1naryth1ef@gmail.com>
2012-11-03 23:53:05 -0500

    Rewrote the basics. Pull HTML from the official GAPI. General fixes

--- app.py
+++ app.py
@@ -1,33 +1,31 @@
 from flask import Flask
-from bs4 import BeautifulSoup
-import urllib, time
+import requests, time, thread, os
 
 app = Flask(__name__)
-cache = None
-cache_time = 0
+cache = {} #Poor Mans Cache
+use_cache = os.getenv('USECACHE', False) #Dont use cache in dev mode
 
-def bs4_slice(web_source):
-    soup = BeautifulSoup(web_source)
-    soup.body.find(id="header").decompose()
-    soup.body.find(id="footer").decompose()
-    for i in soup.body("img"):
-        i["src"] = "https://docs.google.com/document/" + i["src"]
-    return soup.prettify()
-
-def render_doc():
-    try:
-        webpage = urllib.urlopen("https://docs.google.com/document/pub?id=1SGcfQz13ce4FfB-QHKF3WLwxHoCRGBouuvZn-3aoX0k").read()
-        return bs4_slice(webpage)
-    except:
-        return "We're having some technical difficulties. Please check back soon!"
+def fetchDoc(docid):
+    if docid in cache.keys():
+        if time.time()-cache[docid][0] < 300: #5 mins
+            return cache[docid][1]
+    f = requests.get('https://docs.google.com/feeds/download/documents/export/Export', params={'id':docid, 'exportFormat':'html'})
+    if f.status_code != 200:
+        return "We're having issues right now. Please <a href=\"https://images.google.com/?q=pictures+of+bears\">bear</a> with us."
+    if use_cache: cache[docid] = [time.time(), f.text]
+    return f.text
 
 @app.route('/')
 def route_root():
-    global cache, cache_time
-    if time.time()-cache_time > 300:
-        cache = render_doc()
-        cache_time = time.time()
-        return cache
+    return fetchDoc('1SGcfQz13ce4FfB-QHKF3WLwxHoCRGBouuvZn-3aoX0k')
+
+@app.route('/', subdomain='links')
+def link_page():
+    return fetchDoc('1xmKiUVy2vZbluQbtItbN8WMQXPiWCt7f8DVKJe6_w0c')
+
+@app.route('/', subdomain='projects')
+def project_page():
+    return fetchDoc('1wdDo65UcBfdcUTvda5fwb4HOI7RjEvWq3KzZBV9ORcc')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True) #We deploy using gunicorn, so this is safe
